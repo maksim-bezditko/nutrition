7023fafb516e79281c373e899cba8b1a66665ea9
maksim_bezditko <mb.email.for.studying@gmail.com>
2022-06-09 16:47:41 +0300

    modal and test version of slider done

--- js/script.js
+++ js/script.js
@@ -2,8 +2,6 @@
 
 window.addEventListener("DOMContentLoaded", () => {
 
-	// tabs
-
 	const tabConts = document.querySelectorAll(".tabcontent"),
 		   tabs = document.querySelectorAll(".tabheader__item"),
 			tabsParent = document.querySelector(".tabheader__items")
@@ -62,18 +60,25 @@ window.addEventListener("DOMContentLoaded", () => {
 		const date = finalDate.getUTCDate(),
 				month = finalDate.getUTCMonth(),
 				hours = finalDate.getUTCHours(),
-				minutes = finalDate.getUTCMinutes()		
+				minutes = finalDate.getUTCMinutes(),
+				year = finalDate.getUTCFullYear()
 		return {
 			"date": date,
 			"month": months[month],
 			"hours": hours,
-			"minutes": minutes
+			"minutes": minutes,
+			"year": year,
 		}; 
 	}
 	function changeInscription(inscriptionUnits) {
 		const elementOfDOM = document.querySelector("#inscription");
 		
-		let inscription = `Акция закончится ${inscriptionUnits.date} ${inscriptionUnits.month.toLowerCase()} в ${addZero(inscriptionUnits.hours)}:${addZero(inscriptionUnits.minutes)}`;
+		let inscription;
+		if (inscriptionUnits.year == new Date(Date.now()).getFullYear()) {
+			inscription = `Акция закончится ${inscriptionUnits.date} ${inscriptionUnits.month.toLowerCase()} в ${addZero(inscriptionUnits.hours)}:${addZero(inscriptionUnits.minutes)}!`; 
+		} else {
+			inscription = `Акция закончится ${inscriptionUnits.date} ${inscriptionUnits.month.toLowerCase()} в ${addZero(inscriptionUnits.hours)}:${addZero(inscriptionUnits.minutes)} ${inscriptionUnits.year} года!`; 
+		}
 
 		elementOfDOM.textContent = "";
 		elementOfDOM.textContent = inscription;
@@ -95,7 +100,7 @@ window.addEventListener("DOMContentLoaded", () => {
 		const days = Math.floor(diff / (1000 * 60 * 60 * 24)),
 			   hours = Math.floor((diff / (1000 * 60 * 60)) % 24),
 			   minutes = Math.floor((diff / (1000 * 60)) % 60),
-			   seconds = Math.floor((diff / (1000)) % 60);
+			   seconds = Math.floor((diff / (1000)) % 60)
 		
 		return {
 			 'total': diff,
@@ -119,10 +124,13 @@ window.addEventListener("DOMContentLoaded", () => {
 			timer.querySelector("#seconds").innerHTML = addZero(units.seconds); 
 			// console.log(units.total)
 			if (units.total <= 0) {
+				let inscription;
 				clearInterval(timerId);
-
-				let inscription = `Акция закончилась ${inscriptionUnits.date} ${inscriptionUnits.month.toLowerCase()} в ${addZero(inscriptionUnits.hours)}:${addZero(inscriptionUnits.minutes)}!`; 
-
+				if (inscriptionUnits.year == new Date(Date.now()).getFullYear()) {
+					inscription = `Акция закончилась ${inscriptionUnits.date} ${inscriptionUnits.month.toLowerCase()} в ${addZero(inscriptionUnits.hours)}:${addZero(inscriptionUnits.minutes)}!`; 
+				} else {
+					inscription = `Акция закончилась ${inscriptionUnits.date} ${inscriptionUnits.month.toLowerCase()} в ${addZero(inscriptionUnits.hours)}:${addZero(inscriptionUnits.minutes)} ${inscriptionUnits.year} года!`; 
+				}
 				timer.querySelector("#days").innerHTML = "00";
 				timer.querySelector("#hours").innerHTML = "00";
 				timer.querySelector("#minutes").innerHTML = "00";
@@ -175,5 +183,133 @@ window.addEventListener("DOMContentLoaded", () => {
 	// percentage.textContent = `${Math.round(document.documentElement.scrollTop / number * 100)}%`
 	buttonUp.addEventListener("click", moveUp)
 	// console.log(document.documentElement.scrollHeight)
+
+	// Modal
+
+	let btns = document.querySelectorAll("[data-modal]"),
+		 modal = document.querySelector(".modal"),
+		 cross = document.querySelector(".modal__close"),
+		 form = document.querySelector(".modal__content form")
+
+	function showModal() {
+		modal.classList.remove("hide");
+		modal.classList.add("show");
+		document.body.style.overflow = "hidden";
+	}
+
+	function hideModal() {
+		modal.classList.remove("show");
+		modal.classList.add("hide");
+		document.body.style.overflow = "";
+	}
+
+	hideModal()
+
+	btns.forEach((item) => {
+		item.addEventListener("click", () => {
+			showModal()
+			if (modalTimerId) {
+				clearTimeout(modalTimerId)
+			}
+		})
+	})
+
+	cross.addEventListener("click", () => {
+		hideModal()
+	})
+
+	modal.addEventListener("click", event => {
+		if (event.target === document.querySelector(".modal") && modal.classList.contains("show")) {
+			hideModal()
+		}
+	})
+
+	const modalTimerId = setTimeout(() => {
+		showModal()
+	}, 4000)
+
+	function showModalDownThere() {
+		if (document.documentElement.scrollHeight <= document.documentElement.scrollTop + document.documentElement.clientHeight) {
+			showModal()
+		} 
+		if (modalTimerId) {
+			clearTimeout(modalTimerId)
+		}
+	}
+
+	document.addEventListener("scroll", showModalDownThere)
+
+	function changeModalContent() {
+		modal.innerHTML = 
+	  `<div class="modal__dialog">
+			<div class="modal__content">
+				<div class="modal__close">&times;</div>
+				<p class="later">Вы уже заполнили форму! Мы с Вами свяжемся в ближайшее время!</p>
+			</div>
+  		</div>`
+		cross = document.querySelector(".modal__close");
+		cross.addEventListener("click", () => {
+			hideModal()
+		})
+	}
+	
+	form.addEventListener("submit", () => {
+		hideModal()
+		changeModalContent()
+		document.removeEventListener("scroll", showModalDownThere)
+	})
+
+	// Slider test
+
+	let   sliderCounter = document.querySelector("#current"),
+			offerSlides = document.querySelectorAll(".offer__slide"),
+			currentNumber,
+			rigthArrow = document.querySelector(".offer__slider-next"),
+			leftArrow = document.querySelector(".offer__slider-prev")
+
+	function hideSlide(item) {
+		item.classList.remove("show");
+		item.classList.add("hide");
+	}
+
+	function showSlideByItem(item) {
+		item.classList.remove("hide");
+		item.classList.add("show");
+	}
+
+	function showSlideByNumber(i = 0) {
+		hideSlides()
+		sliderCounter.textContent = `${addZero(i + 1)}`
+		showSlideByItem(offerSlides[i])
+		currentNumber = i;
+	}
+
+	function hideSlides() {
+		offerSlides.forEach((item) => {
+			hideSlide(item);
+		})
+	}
+	hideSlides()
+	showSlideByNumber()
+
+	rigthArrow.addEventListener("click", () => {
+		currentNumber += 1;
+		if (currentNumber == 4) {
+			currentNumber -= 4;
+		}
+		showSlideByNumber(currentNumber);
+	})
+
+	leftArrow.addEventListener("click", () => {
+		currentNumber -= 1;
+		if (currentNumber == -1) {
+			currentNumber += 4;
+		}
+		showSlideByNumber(currentNumber);
+	})
+
+
+
+
 
 })
